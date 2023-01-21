10e2a1763d4ba8b114df3aab56039fbbbd02e604
Ali Asgar <jugnu4u.ali@gmail.com>
2023-01-21 21:18:51 +0530

    Initial Commit with Basic Functionality Done for Space shooter game.

---
+++ Assets/_MySpaceShooter/Scripts/EnemySpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfigSO> waveConfigs;
+    [SerializeField] float timeBetweenWaves = 0f;
+    WaveConfigSO currentWave;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemyWaves());
+    }
+
+    public WaveConfigSO GetCurrentWave()
+    {
+        return currentWave;
+    }
+
+    IEnumerator SpawnEnemyWaves()
+    {
+        foreach(WaveConfigSO waves in waveConfigs)
+        {
+            currentWave = waves;
+
+            for (int i = 0; i < currentWave.GetEnemyCount(); ++i)
+            {
+                Instantiate(currentWave.GetEnemyPrefab(i),
+                        currentWave.GetStartingWaypoint().position,
+                        Quaternion.identity, this.transform);
+
+                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+            }
+
+            yield return new WaitForSeconds(timeBetweenWaves);
+        }
+
+        
+    }
+}
---
+++ Assets/_MySpaceShooter/Scripts/Player.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace MySpaceShooter
+{
+    public class Player : MonoBehaviour
+    {
+        Vector2 rawInput;
+        [SerializeField]
+        float moveSpeed = 5.0f;
+
+        Vector2 minBounds;
+        Vector2 maxBounds;
+
+        [SerializeField] float paddingLeft;
+        [SerializeField] float paddingRight;
+        [SerializeField] float paddingTop;
+        [SerializeField] float paddingBottom;
+
+        private void Start()
+        {
+            InitBounds();
+        }
+
+        void InitBounds()
+        {
+            Camera mainCamera = Camera.main;
+            minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+            maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+
+
+        }
+
+        void Update()
+        {
+            Move();
+        }
+
+        void Move()
+        {
+            Vector2 newPos = new Vector2();
+
+            Vector2 delta = rawInput * Time.deltaTime * moveSpeed;
+
+            newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+            newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+
+            transform.position = newPos;
+        }
+
+        private void OnMove(InputValue value)
+        {
+            rawInput = value.Get<Vector2>();
+            Debug.Log(rawInput);
+        }
+    }
+}
+
